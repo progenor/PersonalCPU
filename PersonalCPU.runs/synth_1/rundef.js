7f8010f1be1dfcabc47c3e9c285dccf9c9d57d8d
progenor <nero6126@gmail.com>
2024-11-21 07:58:43 +0200

    tell me why you are wrong

--- PersonalCPU.runs/synth_1/rundef.js
+++ PersonalCPU.runs/synth_1/rundef.js
@@ -24,7 +24,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log CPU.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source CPU.tcl" );
+         "-log toplevel.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source toplevel.tcl" );
 
 
 
--- PersonalCPU.runs/synth_1/runme.sh
+++ PersonalCPU.runs/synth_1/runme.sh
@@ -41,4 +41,4 @@ EAStep()
      fi
 }
 
-EAStep vivado -log CPU.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source CPU.tcl
+EAStep vivado -log toplevel.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source toplevel.tcl
